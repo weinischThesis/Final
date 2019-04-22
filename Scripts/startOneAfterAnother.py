7b6b32da5d7ff38f425484d118b9d12e0d38dcf3
Christoph Weinisch <christoph@weinisch.com>
2019-04-22 17:41:01 +0200

    Final Upload

---
+++ Scripts/startOneAfterAnother.py
@@ -0,0 +1,52 @@
+import os, sys
+if 'SUMO_HOME' in os.environ:
+     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+     sys.path.append(tools)
+else:   
+     sys.exit("please declare environment variable 'SUMO_HOME'")
+
+
+import sumolib
+import mysql.connector
+import math
+import os.path
+import re
+
+def getDuration(carId):
+	outputFile = open('output3.xml', 'r')
+	for line in outputFile:
+		if 'id="'+carId+'"' in line:
+			result = re.search('duration=\"([0-9.]+)\"',line)
+			outputFile.close()
+			return float(result.group(1))
+	outputFile.close()
+	return None
+
+f = open('trip.rou.xml','r')
+f3 = open('startOneAfterTrip.rou.xml','w')
+
+nextStart = 0.0
+lastId = 427
+for line in f:
+	result = re.search('id=\"([0-9]+)\"',line)
+	
+	if result != None:
+		if int(result.group(1)) != lastId +1:
+			lastId = lastId + 1
+			print(lastId)
+		lastId = lastId +1 
+		duration = getDuration(result.group(1))
+		if duration != None:
+			departResult = re.search('depart=\"([0-9.]+)\"',line)
+			newDepart = 'depart="%.2f"' % nextStart
+			newLine = line.replace(departResult.group(0),newDepart)
+			nextStart = duration + nextStart-0.05
+
+	else:
+		newLine = line
+	
+	f3.write(newLine)
+
+f.close()
+
+f3.close()
---
+++ readme.txt
@@ -0,0 +1,21 @@
+MASTER'S THESIS - CHRISTOPH WEINISCH
+
+ROUTE DETERMINATION FOR TELEOPERATED DRIVING BASED ON CELLULAR NETWORK AVAILABILITY	
+
+
+	Approach Comparison 
+	../Comparison
+		SUMO simulation files & Output
+
+
+
+	SCRIPTS ( x-y-updateDB.py, mathematicalAvailable.py)
+
+
+
+	ERLANGEN SIMULATION
+
+	../simulte_veins 
+
+	The subproject can be imported into the OMNeT++ IDE by importing the subprojects/veins_inet folder into the workspace (or by 	selecting the Search for nested projects when importing Veins). 
+	
